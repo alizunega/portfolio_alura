0bc2c9c4d420986f9597b0f074497c2facff7b93
alizunega <alizunega84@gmail.com>
2024-09-18 11:59:44 -0300

    fix formulario, add alerta

--- src/js/app.js
+++ src/js/app.js
@@ -10,23 +10,46 @@ document.addEventListener("click", () => {
     setTimeout(() => {
       menu.hidden = true;
       button.style.display = "block"; // Mostrar el botón de nuevo
-    }, 6000);
+    }, 4000);
+  }
+});
+document.getElementById("contact-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Obtén los valores del formulario
+  let form = document.querySelector("#contact-form");
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let subject = document.getElementById("subject").value;
+  let message = document.getElementById("message").value;
+  let alert = document.querySelector(".alert");
+  if (name && email && message) {
+    // Mostrar mensaje de envio correcto
+    alert.style.display = "block";
+
+    //Muestra el msj por 3 segundos, luego lo cierra
+    setTimeout(() => {
+      alert.style.display = "none";
+      form.reset();
+    }, 3000);
+  } else {
+    // Muestra mensaje de error al enviar
+    alert("El mensaje no pudo enviarse. Intente más tarde.");
   }
 });
 
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+// document
+//   .getElementById("contact-form")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault();
 
-    // Obtén los valores del formulario
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let message = document.getElementById("message").value;
+//     // Obtén los valores del formulario
+//     let name = document.getElementById("name").value;
+//     let email = document.getElementById("email").value;
+//     let message = document.getElementById("message").value;
 
-    // Crea el enlace mailto
-    var mailtoLink = `mailto:ali.zunega@gmail.com?subject=Contacto de ${name}&body=Nombre: ${name}%0AEmail: ${email}%0AMensaje: ${message}`;
+//     // Crea el enlace mailto
+//     var mailtoLink = `mailto:ali.zunega@gmail.com?subject=Contacto de ${name}&body=Nombre: ${name}%0AEmail: ${email}%0AMensaje: ${message}`;
 
-    // Abre el cliente de correo
-    window.location.href = mailtoLink;
-  });
+//     // Abre el cliente de correo
+//     window.location.href = mailtoLink;
+//   });
